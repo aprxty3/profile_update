50131dfb2baaa966c413a9594532da762db0c1f7
aprxrt3 <ajiprxty3@gmail.com>
2021-12-09 15:50:34 +0700

    build home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'theme.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,14 +7,111 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        backgroundColor: bg,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 30,
+                right: 30,
+                top: 60,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Profile Picture',
+                    style: profilePic,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Image.asset(
+                    'assets/primary.png',
+                    width: 140,
+                    height: 140,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Anne Margaritha',
+                        style: nameText,
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        'UX Designer',
+                        style: jobText,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 70,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/1.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                      SizedBox(
+                        width: 38,
+                      ),
+                      Image.asset(
+                        'assets/2.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                      SizedBox(
+                        width: 38,
+                      ),
+                      Image.asset(
+                        'assets/3.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/4.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                      SizedBox(
+                        width: 38,
+                      ),
+                      Image.asset(
+                        'assets/5.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                      SizedBox(
+                        width: 38,
+                      ),
+                      Image.asset(
+                        'assets/6.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 70,),
+                  Container(,)
+                ],
+              ),
+            ),
           ),
         ),
       ),
