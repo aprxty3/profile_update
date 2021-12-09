09d1fc60988ac136f0f1a03addbfe592e718e700
aprxrt3 <ajiprxty3@gmail.com>
2021-12-09 15:50:25 +0700

    create theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TextStyle ColoProfilepic = GoogleFonts.poppins
+Color bg = Color(0xffE5E5E5);
+
+TextStyle profilePic = GoogleFonts.poppins(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  color: Color(0xff314728),
+);
+
+TextStyle nameText = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Color(0xff314728),
+);
+
+TextStyle jobText = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  color: Color(0xffA9B0A6),
+);
+
+TextStyle updateText = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: Color(0xff314728),
+);
