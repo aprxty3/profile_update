b7b1c40c20d99b5e4f426079613276d682d14ef8
aprxrt3 <ajiprxty3@gmail.com>
2021-12-09 19:21:56 +0700

    add modal bottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'theme.dart';
 
 void main() => runApp(MyApp());
@@ -13,104 +14,102 @@ class MyApp extends StatelessWidget {
         backgroundColor: bg,
         body: SingleChildScrollView(
           child: Center(
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 30,
-                right: 30,
-                top: 60,
-                bottom: 30,
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    'Profile Picture',
-                    style: profilePic,
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Image.asset(
-                    'assets/primary.png',
-                    width: 140,
-                    height: 140,
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        'Aji Prasetyo',
-                        style: nameText,
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        'Mobile Engineer',
-                        style: jobText,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 70,
-                  ),
-                  Wrap(
-                    spacing: 38,
-                    runSpacing: 40,
-                    children: [
-                      Image.asset(
-                        'assets/1.png',
-                        width: 80,
-                        height: 80,
-                      ),
-                      Image.asset(
-                        'assets/2.png',
-                        width: 80,
-                        height: 80,
-                      ),
-                      Image.asset(
-                        'assets/3.png',
-                        width: 80,
-                        height: 80,
-                      ),
-                      Image.asset(
-                        'assets/4.png',
-                        width: 80,
-                        height: 80,
-                      ),
-                      Image.asset(
-                        'assets/5.png',
-                        width: 80,
-                        height: 80,
-                      ),
-                      Image.asset(
-                        'assets/6.png',
-                        width: 80,
-                        height: 80,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 70,
-                  ),
-                  Container(
-                    width: 225,
-                    height: 55,
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      color: Colors.white,
-                      onPressed: () {},
-                      child: Text(
-                        'Update Profile',
-                        style: updateText,
-                      ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  'Profile Picture',
+                  style: profilePic,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'assets/primary.png',
+                  width: 140,
+                  height: 140,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Aji Prasetyo',
+                      style: nameText,
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      'Mobile Engineer',
+                      style: jobText,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image.asset(
+                      'assets/1.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/2.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/3.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/4.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/5.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Image.asset(
+                      'assets/6.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  width: 225,
+                  height: 55,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    color: Colors.white,
+                    onPressed: () {},
+                    child: Text(
+                      'Update Profile',
+                      style: updateText,
                     ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 50,
+                )
+              ],
             ),
           ),
         ),
